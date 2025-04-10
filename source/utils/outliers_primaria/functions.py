b1b4333347931051d531c52590f886387010ff53
Marta Huertas <martahuertasp@gmail.com>
2025-04-10 16:10:45 +0200

    solve bugs dates

--- source/utils/outliers_primaria/functions.py
+++ source/utils/outliers_primaria/functions.py
@@ -45,20 +45,18 @@ def remove_date_outliers(df, mortalitat):
     merged_df = pd.merge(df, mortalitat[['codi_p', 'data_defuncio']], on='codi_p', how='left')
 
     # Fill NaN values in 'anaix' with a default value (e.g., 1920) before comparison
-    merged_df['birth_year'] = pd.NA
-    year_minus_100 = datetime.now().year - 100 # Filtering the diagnostics that are possible.
-    merged_df['birth_year'] = merged_df['birth_year'].fillna(year_minus_100).astype(int)
+    merged_df['trust_year'] = 1909 # First birth year in CORDELIA - CAN BE MODIFIED
 
     # Convert the date columns to datetime format
-    merged_df['data_defuncio'] = pd.to_datetime(merged_df['data_defuncio']).dt.tz_localize(None)
-    merged_df['data_ingres'] = pd.to_datetime(merged_df['data_ingres']).dt.tz_localize(None)
-    merged_df['data_alta'] = pd.to_datetime(merged_df['data_alta']).dt.tz_localize(None)
+    merged_df['data_defuncio'] = pd.to_datetime(merged_df['data_defuncio'], errors='coerce', dayfirst=False, format = "%Y-%m-%d")
+    merged_df['data_ingres'] = pd.to_datetime(merged_df['data_ingres'], errors='coerce', dayfirst=False, format = "%Y-%m-%d")
+    merged_df['data_alta'] = pd.to_datetime(merged_df['data_alta'], errors='coerce', dayfirst=False, format = "%Y-%m-%d")
 
     # Apply the first condition: data_ingres can be at most 7 days later than data_defuncio
     condition1 = (merged_df['data_defuncio'].isna()) | (merged_df['data_ingres'] < merged_df['data_defuncio'] + pd.Timedelta(days=7))
 
     # Apply the second condition: any_referencia >= anaix (cast anaix to int)
-    condition2 = merged_df['any_referencia'].astype(int) > merged_df['birth_year']
+    condition2 = merged_df['any_referencia'].astype(int) > merged_df['trust_year']
 
     # Apply the third condition: data_ingres ha de ser abans que la data_alta
     condition3 = (merged_df['data_alta'].isna()) | (merged_df['data_ingres'] < merged_df['data_alta'])
@@ -67,6 +65,31 @@ def remove_date_outliers(df, mortalitat):
     filtered_df = merged_df[condition1 & condition2 & condition3].copy()
 
     # Drop the columns 'data_defuncio' and 'anaix'
-    filtered_df = filtered_df.drop(['data_defuncio', 'birth_year'], axis=1)
+    filtered_df = filtered_df.drop(['data_defuncio', 'trust_year'], axis=1)
 
     return filtered_df
+
+def generate_report(df, entity, report_path, preprocessing_df):
+    """ If --report is on, a report will be generated in the same outpath."""
+    def count_na(df):
+        na_counts = df.isna().sum()
+        total_rows = len(df)
+        for col, na in na_counts.items():
+            pct = (na / total_rows) * 100 if total_rows else 0
+            f.write(f"  - {col}: {na} ({pct:.2f}%)\n\n")
+    
+    with open(report_path, "w", encoding="utf-8") as f:
+        f.write(f"Report for entity: {entity}\n")
+        f.write("-"*50 + "\n")
+        f.write(f"Rows before processing: {len(preprocessing_df)}\n")
+        f.write(f"Rows after processing: {len(df)}\n\n")
+
+        f.write("Missing values per column (before processing):\n")
+        count_na(preprocessing_df)
+
+        f.write("Missing values per column (after processing):\n")
+        count_na(df)
+
+        f.write("\nData types:\n")  # Now works with utf-8!
+        for col, dtype in df.dtypes.items():
+            f.write(f"  - {col}: {dtype}\n")
--- source/utils/outliers_primaria/remove_outliers.py
+++ source/utils/outliers_primaria/remove_outliers.py
@@ -1,7 +1,9 @@
 # Main function to use to remove date and diagnostic outliers in Primaria - PADRIS
 import pandas as pd
-from source.utils.outliers_primaria.functions import *
+from functions import *
 import sys
+import time
+
 
 def main():
     """ Main function to remove primaria outliers."""
@@ -11,18 +13,32 @@ def main():
         print("Usage: python3 main.py <inpath primaria> <inpath mortalitat> <oupath>")
         sys.exit(1)
 
-    primaria, mortalitat, outpath = args[0], args[1], args[2]
-    
+    primaria_path, mortalitat_path, outpath = args[0], args[1], args[2]
+
+    print("Reading input...")
+    # Read csv
+    primaria = pd.read_csv(primaria_path, sep = "|")
+    mortalitat = pd.read_csv(mortalitat_path, sep = "|")
+
     # Read the reference for CIM10 and CIM9
-    cie10_df = pd.read_excel(r".\cie_reference\Diagnosticos_ES2024_TablaReferencia_30_06_2023.xlsx", sheet_name='ES2024 Finales', usecols=['Código'])
-    cie9_df = pd.read_excel(r".\cie_reference\CIE9MC_9_2014_REF_20210601_2362183957514564327.xls", sheet_name='cie9mc2014', usecols=['Tab.D'])
+    cie10_df = pd.read_excel(r"source\utils\outliers_primaria\cie_reference\Diagnosticos_ES2024_TablaReferencia_30_06_2023.xlsx", sheet_name='ES2024 Finales', usecols=['Código'])
+    cie9_df = pd.read_excel(r"source\utils\outliers_primaria\cie_reference\CIE9MC_9_2014_REF_20210601_2362183957514564327.xls", sheet_name='cie9mc2014', usecols=['Tab.D'])
     # From https://www.eciemaps.sanidad.gob.es/documentation at 09/04/2025 -> Tablas de Referencia
 
+    print("Processing primaria...")
     # Remove non coherent cie codes
     primaria_first_filt = remove_non_coherent_cie(primaria, cie9_df, cie10_df)
 
     # Remove non coherent dates
     primaria_filt = remove_date_outliers(primaria_first_filt, mortalitat)
 
+    # Write report 
+    generate_report(primaria_filt, "Primaria", outpath.replace(".csv", "_report.txt"), primaria)
+
     # Output file
     primaria_filt.to_csv(outpath, sep = "|", index = False)
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print(f"--- {time.time() - start_time:.2f} seconds ---")
